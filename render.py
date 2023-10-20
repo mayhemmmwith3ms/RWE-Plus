eeb2e2e29f9ff99ae4b81b333cd2aabe059af005
methylredd <interra361@gmail.com>
2023-10-20 14:15:15 +0800

    improve clarity of TE layers (again...)

--- render.py
+++ render.py
@@ -332,7 +332,7 @@ class Renderer:
                     if i == 2:
                         convrender.set_alpha(ui_settings["global"]["thirdlayeralpha"])
                 else:
-                    convrender.set_alpha(ui_settings["global"]["primarylayeralpha"])
+                    convrender.set_alpha(ui_settings["global"]["primarylayeralpha"] + 30)
             if i == 0 and self.color_geo:
                 convrender.set_alpha(255)
 
