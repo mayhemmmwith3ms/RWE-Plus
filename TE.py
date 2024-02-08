1cf215ceaccd1389a4a5e0bb6f066ef758144d41
methylredd <interra361@gmail.com>
2024-02-08 20:45:52 +0800

    chain holder ughhh

--- TE.py
+++ TE.py
@@ -1066,11 +1066,11 @@ class TE(MenuWithField):
                                [x * self.size, y * self.size])
         for x2 in range(w):
             for y2 in range(h):
-                p = makearr([self.tileimage["cat"][0] + 4, self.tileimage["cat"][1]], "point")  # very bandaid fix, RWE loads tiles and materials in a different order than Drizzle,                                                                         
-                csp = sp[x2 * h + y2]                                                           # so whenever the tilehead category index is actually required for something to function
-                xpos = int(x + x2)                                                              # it Won't, because all of the tile category indices will be wrong
-                ypos = int(y + y2)                                                              # luckily, i don't know of much else that requires the head's category index other than chain holders,
-                if xpos >= self.levelwidth or ypos >= self.levelheight or xpos < 0 or ypos < 0 or (self.data["TE"]["tlMatrix"][xpos][ypos][self.layer]["tp"] == "tileHead"): # which aren't the end of the world if they don't work. this fix should work unless material categories are added or removed
+                p = makearr([self.tileimage["cat"][0], self.tileimage["cat"][1]], "point")                                                                     
+                csp = sp[x2 * h + y2]                                                       
+                xpos = int(x + x2)                                           
+                ypos = int(y + y2)                                        
+                if xpos >= self.levelwidth or ypos >= self.levelheight or xpos < 0 or ypos < 0 or (self.data["TE"]["tlMatrix"][xpos][ypos][self.layer]["tp"] == "tileHead"):
                     continue
                 if self.data["TE"]["tlMatrix"][xpos][ypos][self.layer]["tp"] not in ("default", "tileBody"):
                     self.area[xpos][ypos] = False
