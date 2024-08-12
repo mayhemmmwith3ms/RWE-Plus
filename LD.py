aae91e4b4560ad91fcc6663755eb8f4e9d85f1c8
mayhemmmwith3ms <interra361@gmail.com>
2024-08-12 15:10:23 +0800

    change how version numbers are compared

--- LD.py
+++ LD.py
@@ -18,8 +18,8 @@ class load(Menu):
         try:
             request = requests.get("https://api.github.com/repos/mayhemmmwith3ms/RWE-Plus/releases", timeout=2)
             if request.status_code == 200:
-                gittag = request.json()[0]["tag_name"].split("-")[0].strip("v")
-                if tag != gittag:
+                gittag = request.json()[0]["tag_name"]
+                if tag not in gittag:
                     print("A new version of RWE+ is available!")
                     print(f"Current Version: {tag}, latest: {gittag}")
                     print("https://github.com/mayhemmmwith3ms/RWE-Plus/releases/latest")
