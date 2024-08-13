36c11600d89f596afc38a9f03695a2c93c7325b6
mayhemmmwith3ms <interra361@gmail.com>
2024-08-13 22:43:12 +0800

    finally fix missing icon

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 rmdir /S /Q "%~dp0\dist"
 del /Q "%~dp0\__pycache__"
 rmdir /S /Q "%~dp0\__pycache__"
-pyinstaller --onefile main.py
+pyinstaller --onefile --icon="%~dp0\files\icon.ico" main.py
 xcopy "%~dp0\files" "%~dp0\dist\files" /y /i /s
 del "%~dp0\dist\files\recent.txt"
 xcopy "%~dp0\drizzle" "%~dp0\dist\drizzle" /y /i /s
